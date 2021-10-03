04e8cf823bd249a262059d7d6c9b0e6fa1c6291d
Jonatas Evangelista <jonatas.envangelista@zenklub.com>
2021-10-03 11:49:25 -0300

    creates styles

---
+++ src/app/styles/theme.ts
@@ -0,0 +1,8 @@
+export const theme = {
+  background: '#ffffff',
+  backgroundContrast: '#dddddd',
+  primary: '#df0021',
+  primaryContrast: '#ffffff',
+  secondary: '#333333',
+  secondaryContrast: '#ffffff',
+};
